3308addb2ac1946b060b6b528f59d199dd364157
Brandon Cox <84586515+brandonscottcox@users.noreply.github.com>
2021-06-16 14:44:31 -0600

    Update accounts.js

--- Project_Local_Library_1/public/src/accounts.js
+++ Project_Local_Library_1/public/src/accounts.js
@@ -1,14 +1,10 @@
-//-------------------------------------------------
-
-
-
 function findAccountById(accounts, id) {
   return accounts.find(account => account.id === id) 
 }
 
 
 
-//-------------------------------------------------
+
 
 
 
@@ -20,14 +16,9 @@ function sortAccountsByLastName(accounts) {
 
 
 
-//-------------------------------------------------
 
 
-/*
-function getBorrowsForAccount(accountId, borrows) {
-  return borrows.filter((borrow)=> borrow.id === accountId)
-}
-*/
+
 
 
 
@@ -40,70 +31,6 @@ function getTotalNumberOfBorrows(account, books) {
 }
 
 
-//filter and reduce
-//books.reduce
-//book.borrows.filter => borrow.id === account.id
-//acc + books.borrow.filter.length
-//return books.reduce
-
-//DOES WORK
-/*
-function getTotalNumberOfBorrows(account, books) {
-  const { id: accountId } = account;
-
-  return books.reduce((total, book) => {
-    return (total + book.borrows.filter(borrow => borrow.id === accountId).reduce((accumulatorBorrows) => accumulatorBorrows + 1, 0));
-  }, 0);
-}
-
-                       or
-
-function getTotalNumberOfBorrows(account, books) {
-  const { id: accountId } = account;
-
-  return books.reduce((total, book) => 
-  total + book.borrows.filter(borrow => 
-  borrow.id === accountId).reduce((accumulatorBorrows) => accumulatorBorrows + 1, 0), 0)
-}
-*/
-
-/*
-  let total = 0
-  for (let book of books){
-  const borrows = book.borrows
-  const accountIdBorrows = borrows.filter((borrow)=> borrow.id === account.id)
-  
-  total += accountIdBorrows.length
-  }
-  
-  return total
-}
-*/
-
-
-/*  if (!account || books || books.length === 0) return 0;
-
-const reduce = books.reduce((borrowCount, book)=> {
-  return borrowCount + getBorrowsForAccount(account.id, book.borrows)
-}, 0)
-
-const filter = reduce.filter(x => x.books.borrows === account.id)
-return filter
-*/
-
-/*
-function getTotalNumberOfBorrows(account, books) {
-  const accId = account.id;
-  let total = 0;
-  books.forEach(book => book.borrows.forEach(borrow => accId === borrow.id && total++));
-  return total;
-}
-*/
-
-
-
-
-//-------------------------------------------------
 
 
 
@@ -123,26 +50,7 @@ function getBooksPossessedByAccount(account, books, authors) {
   return booksTaken;
 }
 
-//const filterBooks = books.filter => book.borrows[0].returned === false
-// filterBooks.map => book => {...book, author: books.find => author => book.authorId === author.id}
-/*
-function getBooksPossessedByAccount(account, books, authors) {
-  let booksTaken = [];
-  books.forEach(book=>{
-    if (book.borrows.find(item=>item.id === account.id && !item.returned)) {
-      booksTaken.push(book);
-    }
-  })
-  booksTaken.forEach(book=>{
-    let anAuthor = authors.find(person => person.id === book.authorId);
-    book['author'] = anAuthor;
-  })
-  return booksTaken;
-}
-  */
 
-
-//-------------------------------------------------
 
 
 

4e8f27e8116296c42080104eb33d8eb30c33efc3
Brandon Cox <84586515+brandonscottcox@users.noreply.github.com>
2021-06-16 14:45:35 -0600

    Update books.js

--- Project_Local_Library_1/public/src/books.js
+++ Project_Local_Library_1/public/src/books.js
@@ -1,5 +1,3 @@
-//-------------------------------------------------
-
 function findById(array, id){
   return array.find((element) => element.id === id)
 }
@@ -10,13 +8,6 @@ function findAuthorById(authors, id) {
   return findById(authors, id)
 }
 
-//return authors.find(author => author.id === id)
-
-
-
-
-//-------------------------------------------------
-
 
 
 
@@ -25,10 +16,6 @@ function findAuthorById(authors, id) {
 function findBookById(books, id) {
   return books.find(book=> book.id === id)
 }
-
-
-
-//-------------------------------------------------
 
 
 
@@ -49,15 +36,6 @@ return [borrowed, available]
 
 
 
-//first array of borrowed books => let borrowed =[]
-//second array of borrow books => let avialable = []
-//loop through array check if book is borrowed by checking book.borrows[0].returned
-//if returned available.push
-//if not returned borrowed.push
-//return [borrowed, available]
-
-//-------------------------------------------------
-
 
 
 
@@ -77,72 +55,7 @@ function getBorrowersForBook(book, accounts) {
     });
   
     return combinedBorrows;
-    //return borrows.map => borrow => ({...borrow, ...findAccountById(borrow.id)})
-  }
-
-  /*const capped = book.borrows.slice(0,10)
-const combinedBook = []
-for (let borrow of capped){
-const account =findAccount(accounts, borrow.id)
-const combined = {
-  ...borrow,
-  ...account
 }
-combinedBook.push(combined)
-}
-return combinedBook
-}
-*/
-
-
-//borrows = book.borrows => cap to 10 by using slice
-//return borrows.map => borrow => ({...borrow, ...accountFunctions.findAccountById(borrow.id)})
-
-
-/*
--------------------------------------------
-  const { borrows } = book;
-  const borrowers = borrows.map(({ id, returned })=> {
-    const account = accounts.find(account => account.id === id);
-    return {...account,returned};
-  });
-
-  return borrowers.sort((borrowerA, borrowerB) => {
-      const companyA = borrowerA.id;
-      const companyB = borrowerB.id;
-      return companyA.localeCompare(companyB);
-    }).slice(0, 10);
-}
--------------------------------------------
-
-
-function getBorrowersForBook(book, accounts) {
-  // `borrows` is a list of transactions, each of type { id: string, returned: true }
-  const { borrows } = book;
-
-  const borrowers = borrows.map(({ id, returned })=> {
-    // find account that matches the borrower's ID
-    const account = accounts.find(account => account.id === id);
-
-    // return the matching account, along with the `returned` info
-    return {
-      ...account,
-      returned,
-    };
-  });
-
-  return borrowers
-    .sort((borrowerA, borrowerB) => {
-      const companyA = borrowerA.company;
-      const companyB = borrowerB.company;
-      return companyA.localeCompare(companyB);
-    })
-    .slice(0, 10);
-}
-*/
-
-
-//-------------------------------------------------
 
 
 module.exports = {
